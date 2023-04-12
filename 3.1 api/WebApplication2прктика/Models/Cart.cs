707291808feff4d3ac908c40deeffbb4a05fc5b0
ruoxy <130051280+ruoxy@users.noreply.github.com>
2023-04-13 00:00:59 +0300

    Add files via upload

---
+++ 3.1 api/WebApplication2прктика/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2прктика.Models
+{
+    public partial class Cart
+    {
+        public Cart()
+        {
+            Bookings = new HashSet<Booking>();
+            CartItems = new HashSet<CartItem>();
+        }
+
+        public int CartId { get; set; }
+        public int CustomerId { get; set; }
+
+        public virtual User Customer { get; set; } = null!;
+        public virtual ICollection<Booking> Bookings { get; set; }
+        public virtual ICollection<CartItem> CartItems { get; set; }
+    }
+}
---
+++ 3.1 api/WebApplication2прктика/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2прктика.Models
+{
+    public partial class CartItem
+    {
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public int Amount { get; set; }
+        public int? Price { get; set; }
+
+        public virtual Cart Cart { get; set; } = null!;
+        public virtual Product Product { get; set; } = null!;
+    }
+}
